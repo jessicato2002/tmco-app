7dd24427aceb8c5af1ee75ac859be6014aa6f642
jessicato2002 <jessicato.2002@gmail.com>
2025-01-09 15:19:55 -0800

    added updated files for netlify hosting

--- src/components/SVTHHeader.js
+++ src/components/SVTHHeader.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './SVTHHeader.module.scss';
 import heximage from '../images/heximage.png';
 import OrangeButton from './OrangeButton.js';
+import tf_logo from '../images/tflogo.png';
 
 function SVTHHeader(){
     return(
@@ -17,15 +18,18 @@ helps them acquire new skills and knowledge to be more competitive
 in the market.
                 </p>
                 <div className={styles.learnmore}>
-                    <OrangeButton width='200px' >View Current Courses</OrangeButton>
+                    <OrangeButton width='200px' >View Current Courses</OrangeButton>  
                 </div>
                 <div className={styles.topquote}>
-                    <div className={styles.quotewrapper}>
-                        <div className={styles.orangevertline}></div>
-                        <p className={styles.quote}>“We go from zero to functional in the shortest time possible.”</p>
+                    <img src={tf_logo} alt="tf logo"></img>
+                    <div className={styles.wholequotecont}>
+                        <div className={styles.quotewrapper}>
+                            <div className={styles.orangevertline}></div>
+                            <p className={styles.quote}>“We go from zero to functional in the shortest time possible.”</p>
+                        </div>
+                        <p>Tom Tafolla, J.D</p>   
+                        <p>Owner/Founder </p>
                     </div>
-                    <p>Tom Tafolla, J.D</p>   
-                    <p>Owner/Founder </p>
                 </div>
             </div>
             <div className={styles.cont2}>
